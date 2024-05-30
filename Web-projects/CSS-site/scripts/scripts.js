a7e62b44c8d9029ee313665920b5cb3fd08d137f
gkm <2901997993@qq.com>
2024-05-30 21:21:52 +0800

    看完javascirpts的基础了，感觉就是c++

---
+++ Web-projects/CSS-site/scripts/scripts.js
@@ -0,0 +1,8 @@
+// (var) let const let的值可以被修改 
+let age =  30;
+const name = "gkm";
+age= 31;
+console.log(age);
+//string number boolean null undefined js中没有浮点数的概念，都是浮点数
+console.log(typeof age);
+console.log(`my name is ${name} and i am ${age}`);//使用的是esc下面的反引号不是单引号
