8bc540571b7ec111699ea9eab3f260f94573800c
gkm <2901997993@qq.com>
2024-06-03 20:19:30 +0800

    练习

--- react-intro-exercise/src/App.js
+++ react-intro-exercise/src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+//引入react是为了使得所有的JSX语句能转为React.createElement()
+//src中存储源码，并嵌入到public中的index.html，从而使得浏览器上能够显示出来
+//JSX语句就是HTML+JS
+//
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You are welcome！
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- react-intro-exercise/src/index.js
+++ react-intro-exercise/src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//我的理解，dom其实就是html中渲染组件的位置
+//这里的root的意思是作为第一个组件来渲染我们的reactapp
+//react本质是一个库，但口头上也可以用框架来替代
+//
 root.render(
   <React.StrictMode>
     <App />
